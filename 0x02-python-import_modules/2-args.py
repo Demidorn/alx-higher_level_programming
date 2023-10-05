e784692ec90d85fadcc7eaa59dab375414651769
Demidorn <dornkaizen@gmail.com>
2023-10-05 06:42:10 -0700

    prints the number of and the list of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if (len(sys.argv) - 1) != 1:
+        print("{} arguments".format(len(sys.argv) - 1), end='')
+    else:
+        print("{} argument".format(len(sys.argv) - 1), end='')
+    if len(sys.argv) == 1:
+        print(".")
+    else:
+        print(":")
+    for i, argument in enumerate(sys.argv):
+        if (i == 0):
+            continue
+        print("{}: {}".format((i), argument))
