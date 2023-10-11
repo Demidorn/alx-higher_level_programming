a8875ad73bcbb2c2a96e3be7be7d3fc681c3eebc
Demidorn <dornkaizen@gmail.com>
2023-10-11 17:47:04 +0300

    replaces all occurrences  by another in a new list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,5 +3,5 @@
 def search_replace(my_list, search, replace):
     new_list = []
     if my_list:
-        new_list = list(map(lamba x: x if x != search else replace, my_list))
+        new_list = list(map(lambda x: x if x != search else replace, my_list))
     return new_list
