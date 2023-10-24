bf22b39812a09a48d230253bb64490979e9bd1fc
Demidorn <dornkaizen@gmail.com>
2023-10-24 09:53:53 -0700

    defines a square with private instance attribute and instatiation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+class Square:
+    """Private instance attribute and instantation with optional
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size): Constructor method to initialize a Square instance.
+    """
+
+    def __init__(self, size = 0):
+        """
+        Initialize a Square instance.
+
+        Args:
+            size (int): The size of the square.
+
+        Attributes:
+            __size (int): The size of the square.
+        """
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
