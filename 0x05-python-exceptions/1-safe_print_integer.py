23952c528aefe980e8817c5172c3ad5a1bce7fa4
Demidorn <dornkaizen@gmail.com>
2023-10-23 19:58:41 -0700

    prints x elements of a list

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+    result = False
     try:
         print("{:d}".format(value))
-        return = True
+        result = True
     except:
-        return = False
+        result = False
+    return (result)
