de15b885aba88ab7331294c1c8814907080c0c78
Demidorn <dornkaizen@gmail.com>
2023-11-03 02:15:29 -0700

    adds 2 integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,12 +3,13 @@
 
 def add_integer(a, b=98):
     """Returns the addition of a and b,
-    or an error if a and b are not integers or floats
+        or an error if a and b are not integers or floats
     """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
+
     """Calculate the sum and return it as an integer"""
     result = int(a) + int(b)
     return result
