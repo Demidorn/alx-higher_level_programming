8ec352c1df5bc2202974868328ee7f4bb9f73a51
Demidorn <dornkaizen@gmail.com>
2023-11-06 09:06:34 -0800

    function with exception error

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defines base class BaseGeometry"""
+
+
+class BaseGeometry:
+    """ sub class"""
+    def area(self):
+        """raises message with error"""
+        return Exception("area() is not implemented")
+
