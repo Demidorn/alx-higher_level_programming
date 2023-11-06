a1cee772c762fc02794d3f441414ddb8a15623f8
Demidorn <dornkaizen@gmail.com>
2023-11-06 06:45:59 -0800

    class inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" new class that inherites its attributes from list class"""
+class MyList(list):
+    """ initialization of function that prints
+    all atributes inherited from the list base class
+    """
+
+    def print_sorted(self):
+        """variable to sort list in ascending order"""
+        new_list = sorted(self)
+        print(new_list)
