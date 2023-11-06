b92badbfdbda0b1cd366007c161d53efb18e4af0
Demidorn <dornkaizen@gmail.com>
2023-11-06 06:58:13 -0800

    checks  instance of the specified class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+"""
+function checks for instance nature of the object
+if it's the exact specified instance, it return
+true otherwise returns false
+"""
+def is_same_class(obj, a_class):
+    """ checking if instance is as specified"""
+    return type(obj) is a_class
