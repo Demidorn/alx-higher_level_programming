63bc42a7a8c184971248a04d66ee43c22d12bc61
Demidorn <dornkaizen@gmail.com>
2023-11-07 01:14:45 -0800

    adds new attribute

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -8,5 +8,5 @@
 
 
 def is_kind_of_class(obj, a_class):
-     """ is obj kind of class """
-     return isinstance(obj, a_class)
+    """ is obj kind of class """
+    return isinstance(obj, a_class)
