1600639f6f87baf1d97b76aec416439c992361e0
Demidorn <dornkaizen@gmail.com>
2023-11-08 01:10:49 -0800

    writes a string to a text file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" a function that writes a string to a text file
+and returns number of characters written """
+
+
+def write_file(filename="", text=""):
+    """ read file """
+    with open(filename, 'r', encoding='utf-8') as f:
+        xters = f.write(text)
+    return xters
