b194123311642daf2bb1a51b4447e856506237cf
Demidorn <dornkaizen@gmail.com>
2023-11-08 05:58:47 -0800

    new class student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,11 +9,12 @@ described by;
 
 
 class Student:
-    """ initialization """
+    """student class"""
     def __init__(self, first_name, last_name, age):
-        self.__first_name = first_name
-        self.__last_name = last_name
-        self.__age = age
+        """ initialization of student obj """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """ retrieves a dictionary representation """
