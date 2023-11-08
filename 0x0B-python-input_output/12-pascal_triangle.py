c48483dee65c328c5fd9e7d131c63f3c0adabe1b
Demidorn <dornkaizen@gmail.com>
2023-11-08 06:32:33 -0800

    pascal_triangle that returns lists of lists

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -7,20 +7,15 @@ return lists of lists of integers
 
 
 def pascal_triangle(n):
-    """
-    return list of lists of int
-    """
-    tri_list = [[]]
+    """ makes a list of lists representing a pascal tringle """
+    lista = []
     if n <= 0:
-        return tri_list
-    for j in range(1, n + 1):
-        """"initialization of list"""
-        tri_list.append([])
-        for ele in range(li):
-            """ checking object representation"""
-            if ele == 0 or ele == j - 1:
-                tri_list[j].append(1)
-            else:
-                tri_list[j].append(tri_list[j-1][ele] + tri_list[j-1][ele-1])
-
-    return tri_list[1:]
+        return lista
+    if n >= 1:
+        lista.append([1])
+    for i in range(1, n):
+        lista.append([1])
+        for j in range(len(lista[i - 1]) - 1):
+            lista[i].append(lista[i - 1][j] + lista[i - 1][j + 1])
+        lista[i].append(1)
+    return lista
