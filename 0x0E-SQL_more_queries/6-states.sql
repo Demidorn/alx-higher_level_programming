a53bb8b50098356d3b7b5d710a86ed80fe7f9ea7
Demidorn <dornkaizen@gmail.com>
2023-12-13 23:58:23 -0800

    creates the database hbtn_0d_usa and the table states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates table in created database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+	id INT AUTO_INCREMENT UNIQUENOT NULL PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+	);
