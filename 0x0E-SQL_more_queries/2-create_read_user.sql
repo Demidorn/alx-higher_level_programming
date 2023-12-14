14ccd7020b7ab795bfddff26d355d54e558f2f35
Demidorn <dornkaizen@gmail.com>
2023-12-13 23:15:29 -0800

    creates database & user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
--- This script creates a database and user
+-- This script creates database and user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NO EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CTREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
