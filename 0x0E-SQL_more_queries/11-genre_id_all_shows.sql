db220c0fb2c25eb31c437412220e19ff74af307b
Demidorn <dornkaizen@gmail.com>
2023-12-14 01:29:42 -0800

    displays tv shows in a database

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- a script that lists all shows contained in the database hbtn_0d_tvshows
 SELECT tv_shows.title, COALESCE(tv_show_genres.genre_id , 'NULL')
-FROM tv_show_genres
-RIGHT INNER JOIN tv_shows
-ON tv_show_genres.show_id = tv_shows.id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
