20fb1114d1584275e5788f0fd27c0a369516e957
Demidorn <dornkaizen@gmail.com>
2024-01-09 01:04:30 +0300

    prints x times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const arg =  process.argv[2];
+const numOfOcurrances = parseInt(arg);
+
+if (!isNaN(numOfOcurrances)){
+    for (i = 0; i < numOfOcurrances; i++){
+        console.log("C is fun");
+    }
+} else {
+    console.log("Missing number of occurrences");
+}
+
