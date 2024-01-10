9c0d6ae3eb07f07c4161b4050a031319fb342bdf
Demidorn <dornkaizen@gmail.com>
2024-01-10 01:48:26 -0800

    computes and prints a factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
 const factorial = (n) => {
-  if (isNaN(n)) {
+  if (isNaN(n) || n < 0) {
     return 1;
   }
-  if (n <= 1) {
+  if (n === 1) {
     return 1;
   }
   return n * factorial(n - 1);
 };
 
-module.exports = { factorial };
-if (require.main === modules) {
-  const arg = process.argv[2];
-  const num = parseInt(arg);
+/*module.exports = { factorial };
+if (require.main === modules) {*/
+const arg = process.argv[2];
+const num = parseInt(arg);
 
 console.log(`${factorial(num)}`);
-}
