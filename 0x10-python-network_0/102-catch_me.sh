087f3eeb47ca74448c2152dc9be01b792ae14a77
Demidorn <dornkaizen@gmail.com>
2024-01-25 23:31:27 -0800

    bash scripts on python network basics

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script that makes a request to causes an specific response
-curl -s -L 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:X-School"
+curl -s -X PUT -d "user_id=98" -L -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
