5814c911a4e544b5e0aa3709f0449480169c6a75
Demidorn <dornkaizen@gmail.com>
2024-01-30 03:15:13 -0800

    posts email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ import sys
 
 
 if __name__ == "__main__":
-    values = {'email':sys.argv[2]}
+    values = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(values)
     data = data.encode('ascii')
     req = urllib.request.Request(sys.argv[1], data)
