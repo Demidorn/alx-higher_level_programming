2e0b73ddb7db89cf73cb14fa97bebbe5facf0d08
Demidorn <dornkaizen@gmail.com>
2024-01-30 02:30:52 -0800

    a Python script that fetches a response from url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,10 @@ if __name__ == "__main__":
     with urllib.request.urlopen(
             'https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
-    print("Body response:\n\t- type:{}".format(type(html)))
-    print("\t- content:{}".format(html))
-    print("\t- utf8 content:{}".format(html.decode("utf-8")))
+
+    content_str = html.decode("utf-8")
+
+    print("Body response:")
+    print(f"- type: {type(html)}")
+    print(f"- content: {repr(html)}")
+    print(f"- utf8 content: {content_str}")
