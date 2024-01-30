36680ca3c9960695a1ce3f6581ed9e17f5e2a48e
Demidorn <dornkaizen@gmail.com>
2024-01-30 03:20:49 -0800

    prints http response error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,15 +2,15 @@
 """ a python script that prints html status error"""
 
 
-import urllib.request
-import urllib.error
+from urllib import request, error
 import sys
 
 
 if __name__ == "__main__":
-    requeststatus = urllib.request.Request(sys.argv[1])
+    argv = sys.argv
+    url = argv[1]
     try:
-        with urllib.request.urlopen(requeststatus) as response:
+        with request.urlopen(url) as response:
             print(response.read().decode('utf-8'))
-    except urllib.error.URLError as e:
-            print("Error code:{}".format(e.code))
+    except error.HTTPError as err:
+        print("Error code: {}".format(err.status))
