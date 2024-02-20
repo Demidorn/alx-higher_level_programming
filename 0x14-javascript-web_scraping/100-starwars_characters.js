78fdd9b50c0c7887aef9ad34ab20989b242373af
Demidorn <dornkaizen@gmail.com>
2024-02-20 12:10:43 -0800

    web scrapping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,24 +1,24 @@
 #!/usr/bin/node
 const request = require('require');
-const apiURL = `https://swapi-api.alx-tools.com/api/films/$process.argv[2]/`;
+const apiURL = 'https://swapi-api.alx-tools.com/api/films/$process.argv[2]/';
 request.get(apiURL, (error, response, body) => {
-	if (error) {
-		console.error('Error making the request:', error);
-	}
-	try {
-		const movieData = JSON.parse(body);
-		console.log(`characters for Movie ID ${process.argv[2]} - "${movieData.title}":`);
-		movieData.characters.forEach(characterUrl => {
-			request.get(characterUrl, (charError, charResponse, charBody) => {
-				if (charError) {
-					console.error('Error fetching character data:', charError);
-				} else {
-					const characterData = JSON.parse(charBody);
-					console.log(characterData.name);
-				}
-			});
-		});
-	} catch (parseError) {
-		console.error('Error parsing the API response:', parseError);
-	}
+  if (error) {
+    console.error('Error making the request:', error);
+  }
+  try {
+    const movieData = JSON.parse(body);
+    console.log(`characters for Movie ID ${process.argv[2]} - "${movieData.title}":`);
+    movieData.characters.forEach(characterUrl => {
+      request.get(characterUrl, (charError, charResponse, charBody) => {
+        if (charError) {
+          console.error('Error fetching character data:', charError);
+        } else {
+          const characterData = JSON.parse(charBody);
+          console.log(characterData.name);
+        }
+      });
+    });
+  } catch (parseError) {
+    console.error('Error parsing the API response:', parseError);
+  }
 });
--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,13 +2,13 @@
 const request = require('request');
 const fs = require('fs');
 request.get(process.argv[2], (error, response, body) => {
-	if (error) {
-		console.log(error);
-	} else {
-		fs.writeFile(process.argv[3], body, 'utf8', (error) => {
-			if (error) {
-				console.log(error);
-			}
-		});
-	}
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf8', (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
 });
